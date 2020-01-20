94e8711787ccf54c1f824fcfba6df926c372f7af
Patrick Kille <pr.kille@gmail.com>
2020-01-20 09:58:17 -0800

    clarify variable names, create fully functional js and add README

---
+++ README.md
@@ -0,0 +1,23 @@
+# _Triangle Selector_
+
+#### _This page will show what kind of triangle three lenghts would make, 1/20/20_
+
+#### By _**Patrick Kille**_
+
+## Description
+
+This webpage asks the user to input the side lengths of a triangle and returns the type of triangle it would create.
+
+## Setup/Installation Requirements
+
+* _First, clone this repository to your desktop by opening your terminal and entering the prompt "cd desktop" followed by pressing the enter(return) key. Then type "git clone" followed by the URL to this repository and press enter(return) again._
+* _Navigate to the programming-language-selector folder in either your terminal or by double clicking the folder on your desktop._
+* _Next open the triangle.html file in any web browser you choose._
+
+## Technologies Used
+
+_Git, HTML, CSS (Bootstrap), JavaScript (jQuery)_
+
+### License
+
+Copyright (c) 2020 **_Patrick Kille_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
 $(document).ready(function() {
   $("form#add").submit(function(event) {
-    var sideA = parseInt($("input#add1").val());
-    var sideB = parseInt($("input#add2").val());
-    var sideC = parseInt($("input#add3").val());
-if (sideA === sideB && sideB === sideC) {
-  $("#equalateral").show();
-} else if (sideA === sideB && sideB != sideC) {
-  $("#isocele").show();
-} else if (sideA != sideB && sideB === sideC) {
-  $("#scalene").show();
-}  else {
-  $("#not-triangle").show();
-}
-
-
-
+    var sideA = parseInt($("input#sideA").val());
+    var sideB = parseInt($("input#sideB").val());
+    var sideC = parseInt($("input#sideC").val());
+    $("#equilateral, isoceles, scalene, not-triangle").hide();
+    if (sideA + sideB <= sideC || sideB + sideC <= sideA || sideA + sideC <= sideB) {
+      $("#not-triangle").show();
+    } else if (sideA === sideB && sideB === sideC) {
+      $("#equilateral").show();
+    } else if (sideA === sideB && sideB != sideC) {
+      $("#isoceles").show();
+    } else if (sideB === sideC && sideA != sideB) {
+      $("#isoceles").show();
+    } else if (sideA === sideC && sideC != sideB) {
+      $("#isoceles").show();
+    } else if (sideB != sideC && sideA != sideB) {
+      $("#scalene").show();
+    } else if (sideA != sideC && sideC != sideB) {
+      $("#scalene").show();
+    } else if (sideA != sideB && sideB != sideC) {
+      $("#scalene").show();
+    }
 event.preventDefault();
 
   });
